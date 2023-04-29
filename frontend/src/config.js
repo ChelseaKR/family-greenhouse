6222ac69e0aebdbfcf0042020ef3be9d7f13404a
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-04-29 15:15:51 -0700

    consolidated env vars

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,12 @@
-import configJson from "./auth_config.json";
-
 export function getConfig() {
   const audience =
-    configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
-      ? configJson.audience
+    process.env.REACT_APP_AUTH0_AUDIENCE && process.env.REACT_APP_AUTH0_AUDIENCE !== "YOUR_API_IDENTIFIER"
+      ? process.env.REACT_APP_AUTH0_AUDIENCE
       : null;
 
   return {
-    domain: configJson.domain,
-    clientId: configJson.clientId,
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    clientId: process.env.REACT_APP_AUTH0_CLIENTID,
     ...(audience ? { audience } : null),
   };
 }
