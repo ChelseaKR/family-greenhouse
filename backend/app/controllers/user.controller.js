d41f8e13da23f5a25a3395f1ab250462c1edcc30
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-07 13:01:04 -0700

    cleanup

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1,6 +1,3 @@
-const crypto = require("crypto");
-const { response } = require("express");
-const AuthenticationClient = require('auth0').AuthenticationClient;
 const ManagementClient = require('auth0').ManagementClient;
 
 const auth0Management = new ManagementClient({
@@ -28,7 +25,7 @@ exports.create = (req, res) => {
         }
         return result;
     }
-    console.log(req.body.greenhouseId);
+
     // Create a user
     const metadata = {'greenhouse': req.body.greenhouseId};
     const options = {
@@ -39,7 +36,6 @@ exports.create = (req, res) => {
         verify_email: true,
     };
 
-
     try {
         const user = auth0Management.createUser(options);
     } catch (error) {
@@ -48,90 +44,25 @@ exports.create = (req, res) => {
 };
 
 
-// Find a single Plant with an id
+// Find a single User with an id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
+    const id = req.body.id;
 
-    Plant.findByPk(id)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving Plant with id=" + id
-            });
-        });
 };
 
 // Retrieve all Users from the database.
 exports.findAll = (req, res) => {
     const greenhouseId = req.query.greenhouseId;
+    // Create a user
+    const metadata = {'greenhouse': req.body.greenhouseId};
 
+    try {
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-// Update a Plant by the id in the request
-exports.update = (req, res) => {
-    const id = req.params.id;
-
-    Plant.update(req.body, {
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Plant was updated successfully."
-                });
-            } else {
-                res.send({
-                    message: `Cannot update Plant with id=${id}. Maybe Plant was not found or req.body is empty!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating Plant with id=" + id
-            });
-        });
-};
-
-// Delete a Plant with the specified id in the request
+// Delete a User with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
-
-    Plant.destroy({
-        where: { id: id }
-    })
-        .then(num => {
-            if (num == 1) {
-                res.send({
-                    message: "Plant was deleted successfully!"
-                });
-            } else {
-                res.send({
-                    message: `Cannot delete Plant with id=${id}. Maybe Plant was not found!`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Could not delete Plant with id=" + id
-            });
-        });
-};
-
-// Delete all Plants from the database.
-exports.deleteAll = (req, res) => {
-    Plant.destroy({
-        where: {},
-        truncate: false
-    })
-        .then(nums => {
-            res.send({ message: `${nums} Plants were deleted successfully!` });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while removing all Plants."
-            });
-        });
 };
