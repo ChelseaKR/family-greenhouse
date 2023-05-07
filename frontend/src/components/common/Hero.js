d9bb96a0b2e27bdb4dda29be0de8ad58d87669d5
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-06 23:08:41 -0700

    signup button

--- frontend/src/components/common/Hero.js
+++ frontend/src/components/common/Hero.js
@@ -1,16 +1,37 @@
 import React from "react";
+import { Button } from "reactstrap";
+import { useAuth0 } from '@auth0/auth0-react';
 
 import logo from "../../assets/logo.svg";
 
-const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="Family Greenhouse" width="120" />
-    <h1 className="mb-4">Family Greenhouse</h1>
+const Hero = () => {
+    const { loginWithRedirect } = useAuth0();
 
-    <p className="lead">
-        Grow together effortlessly with our family-friendly plant care app.
-    </p>
-  </div>
-);
+    const handleSignUp = () => {
+        loginWithRedirect({
+            screen_hint: 'signup'
+        });
+    };
+
+    return (
+        <div className="text-center hero my-5">
+            <img className="mb-3 app-logo" src={logo} alt="Family Greenhouse" width="120" />
+            <h1 className="mb-4">Family Greenhouse</h1>
+
+            <p className="lead">
+                Grow together effortlessly with our family-friendly plant care app.
+                <br />
+                <br/>
+                <Button
+                    color="primary"
+                    className="btn-margin"
+                    onClick={handleSignUp}
+                >
+                    Sign up
+                </Button>
+            </p>
+        </div>
+    );
+};
 
 export default Hero;
