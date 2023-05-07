0999ea831f03782e44de019583911badaaf059d4
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-06 18:22:07 -0700

    working build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
-
 cd $(git rev-parse --show-toplevel)
 npm --prefix=frontend run build
-npm --prefix=backend run build
 
-mv frontend/build backend/dist
+# Run backend build process if needed, but not with react-scripts
+
+# Create backend/dist directory if it doesn't exist
+mkdir -p backend/dist
+
+# Move frontend build output to backend/dist
+mv frontend/build/* backend/dist/
