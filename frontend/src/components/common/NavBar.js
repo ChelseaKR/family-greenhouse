17935e702a3ea7072a13c40639efdab2063a8faf
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-06 22:39:38 -0700

    rewrite

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import logo from "../../assets/logo.svg"
+import logo from "../../assets/logo.svg";
 
-import {
-  Collapse,
+import { Collapse,
   Container,
   Navbar,
   NavbarToggler,
@@ -32,153 +31,96 @@ const NavBar = () => {
   const toggle = () => setIsOpen(!isOpen);
 
   const logoutWithRedirect = () =>
-    logout({
+      logout({
         logoutParams: {
           returnTo: window.location.origin,
-        }
-    });
+        },
+      });
 
   return (
-    <div className="nav-container">
-      <Navbar color="light" light expand="md">
-        <Container>
-          <NavbarToggler onClick={toggle} />
-          <NavbarBrand className="logo" />
-          <a className="navbar-brand" href="/">
-            <img src={logo} width="40" height="30"
-                 className="d-inline-block align-top" alt=""/>
-            Family Greenhouse
-          </a>
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-{/*              <NavItem>
-                <NavLink
-                  tag={RouterNavLink}
-                  to="/"
-                  exact
-                  activeClassName="router-link-exact-active"
-                >
-                  Home
-                </NavLink>
-              </NavItem>*/}
-            </Nav>
-            <Nav className="d-none d-md-block" navbar>
-              {!isAuthenticated && (
-                <NavItem>
-                  <Button
-                    id="qsLoginBtn"
-                    color="primary"
-                    className="btn-margin"
-                    onClick={() => loginWithRedirect()}
-                  >
-                    Log in
-                  </Button>
-                </NavItem>
-              )}
-              {isAuthenticated && (
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret id="settingsDropDown">
-                    <img
-                      src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile rounded-circle"
-                      width="50"
-                    />
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem header>{user.name}</DropdownItem>
-                    <DropdownItem
-                        tag={RouterNavLink}
-                        to="/greenhouse"
-                        className="dropdown-profile"
-                        activeClassName="router-link-exact-active"
-                    >
-                      <FontAwesomeIcon icon="house-chimney" className="mr-3" />My Greenhouse
-                    </DropdownItem>
-                    <DropdownItem
-                      tag={RouterNavLink}
-                      to="/profile"
-                      className="dropdown-profile"
-                      activeClassName="router-link-exact-active"
-                    >
-                      <FontAwesomeIcon icon="user" className="mr-3" /> Settings
-                    </DropdownItem>
-                    <DropdownItem
-                      id="qsLogoutBtn"
-                      onClick={() => logoutWithRedirect()}
-                    >
-                      <FontAwesomeIcon icon="power-off" className="mr-3" /> Log
-                      out
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              )}
-            </Nav>
-            {!isAuthenticated && (
-              <Nav className="d-md-none" navbar>
-                <NavItem>
-                  <Button
-                    id="qsLoginBtn"
-                    color="primary"
-                    block
-                    onClick={() => loginWithRedirect({})}
-                  >
-                    Log in
-                  </Button>
-                </NavItem>
+      <div className="nav-container">
+        <Navbar color="light" light expand="md">
+          <Container className="d-flex justify-content-between">
+            <NavbarBrand className="logo" href="/">
+              <img
+                  src={logo}
+                  width="40"
+                  height="30"
+                  alt=""
+              />
+              Family Greenhouse
+            </NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                {isAuthenticated && (
+                    <NavItem>
+                      <NavLink
+                          tag={RouterNavLink}
+                          to="/agenda"
+                          exact
+                          activeClassName="router-link-exact-active"
+                      >
+                        Agenda
+                      </NavLink>
+                    </NavItem>
+                )}
               </Nav>
-            )}
-            {isAuthenticated && (
-              <Nav
-                className="d-md-none justify-content-between"
-                navbar
-                style={{ minHeight: 170 }}
-              >
-                <NavItem>
-                  <span className="user-info">
-                    <img
-                      src={user.picture}
-                      alt="Profile"
-                      className="nav-user-profile d-inline-block rounded-circle mr-3"
-                      width="50"
-                    />
-                    <h6 className="d-inline-block">{user.name}</h6>
-                  </span>
-                </NavItem>
-                <NavItem>
-                <FontAwesomeIcon icon="house-chimney" className="mr-3" />
-                <RouterNavLink
-                    to="/greenhouse"
-                    activeClassName="router-link-exact-active"
-                >
-                  My Greenhouse
-                </RouterNavLink>
-              </NavItem>
-                <NavItem>
-                  <FontAwesomeIcon icon="user" className="mr-3" />
-                  <RouterNavLink
-                    to="/profile"
-                    activeClassName="router-link-exact-active"
-                  >
-                    Profile
-                  </RouterNavLink>
-                </NavItem>
-                <NavItem>
-                  <FontAwesomeIcon icon="power-off" className="mr-3" />
-                  <RouterNavLink
-                    to="#"
-                    id="qsLogoutBtn"
-                    onClick={() => logoutWithRedirect()}
-                  >
-                    Log out
-                  </RouterNavLink>
-                </NavItem>
+              <Nav navbar>
+                {!isAuthenticated ? (
+                    <NavItem>
+                      <Button
+                          id="qsLoginBtn"
+                          color="primary"
+                          className="btn-margin"
+                          onClick={() => loginWithRedirect()}
+                      >
+                        Log in
+                      </Button>
+                    </NavItem>
+                ) : (
+                    <UncontrolledDropdown nav inNavbar>
+                      <DropdownToggle nav caret id="profileDropDown">
+                        <img
+                            src={user.picture}
+                            alt="Profile"
+                            className="nav-user-profile rounded-circle"
+                            width="35"
+                        />
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>{user.name}</DropdownItem>
+                        <DropdownItem
+                            tag={RouterNavLink}
+                            to="/greenhouse"
+                            className="dropdown-profile"
+                            activeClassName="router-link-exact-active"
+                        >
+                          <FontAwesomeIcon icon="house-chimney" className="mr-3" />My Greenhouse
+                        </DropdownItem>
+                        <DropdownItem
+                            tag={RouterNavLink}
+                            to="/profile"
+                            className="dropdown-profile"
+                            activeClassName="router-link-exact-active"
+                        >
+                          <FontAwesomeIcon icon="user" className="mr-3" /> Profile
+                        </DropdownItem>
+                        <DropdownItem
+                            id="qsLogoutBtn"
+                            onClick={() => logoutWithRedirect()}
+                        >
+                          <FontAwesomeIcon icon="power-off" className="mr-3" /> Log
+                          out
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                )}
               </Nav>
-            )}
-          </Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
   );
 };
 
