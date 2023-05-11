eb5ca2feea36f2ea6afe82de9befd1efa14be841
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-10 22:19:45 -0700

    added history jest tests

--- backend/app/controllers/history.controller.js
+++ backend/app/controllers/history.controller.js
@@ -25,7 +25,7 @@ exports.findAll = (req, res) => {
     })
     .catch((err) => {
         res.status(500).send({
-            message: err.message || "Some error occurred while retrieving Histories.",
+            message: err.message || "Some error occurred while retrieving history.",
         });
     });
 };
