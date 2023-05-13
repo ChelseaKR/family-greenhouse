72b149adff83aa2590e7f5e26cff14c7d8e5357e
Chelsea Kelly-Reif (she/her) <ckellyreif@gmail.com>
2023-05-12 17:35:21 -0700

    add isCompleted check

--- frontend/src/components/history/history-item.js
+++ frontend/src/components/history/history-item.js
@@ -1,7 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const HistoryItem = ({ event, index, currentIndex, onSetActive }) => {
+    const [isCompleted, setIsCompleted] = useState(event.is_completed);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleCheckboxChange = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.put(`${process.env.REACT_APP_APP_API_URL}/history/${event.id}`, { is_completed: !isCompleted });
+            setIsCompleted(!isCompleted);
+            setError(null);
+        } catch (err) {
+            setError(err.response?.data?.message || 'An unspecified error occurred');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     if (!event) {
         return (
             <tr>
@@ -12,18 +29,28 @@ const HistoryItem = ({ event, index, currentIndex, onSetActive }) => {
 
     console.log(JSON.stringify(event));
     return (
-        <tr
-            className={index === currentIndex ? "table-active" : ""}
-            onClick={() => onSetActive(event, index)}
-        >
-            <td>{event.datetime.slice(0, 10)}</td>
-            <td>{event.task ? event.task.task_type : ""}</td>
-            <td>{event.task.plant ? event.task.plant.name : ""}</td>
-            <td>{event.task.plant ? event.task.plant.type : ""}</td>
-            <td>{event.is_completed ? "Yes" : "No"}</td>
-            <td>{event.completed_by}</td>
-            <td>{event.date_completed}</td>
-        </tr>
+        <>
+            {error && <div>{error}</div>}
+            <tr
+                className={index === currentIndex ? "table-active" : ""}
+                onClick={() => onSetActive(event, index)}
+            >
+                <td>{event.datetime.slice(0, 10)}</td>
+                <td>{event.task ? event.task.task_type : ""}</td>
+                <td>{event.task.plant ? event.task.plant.name : ""}</td>
+                <td>{event.task.plant ? event.task.plant.type : ""}</td>
+                <td>
+                    <input
+                        type="checkbox"
+                        checked={isCompleted}
+                        disabled={isLoading}
+                        onChange={handleCheckboxChange}
+                    />
+                </td>
+                <td>{event.completed_by}</td>
+                <td>{event.date_completed}</td>
+            </tr>
+        </>
     );
 };
 
